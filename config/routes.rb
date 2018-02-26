cdf20f1434c183840525c274255921e116b5dcea
[pan16] <[mnsaabc@yahoo.co.jp]>
2018-02-26 15:43:00 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '/',    to: 'static_pages#home',    via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/user/sign_up',    to: 'users#new',    via: 'get'
+  match '/user',    to: 'users#create',    via: 'post'
+  match '/user/update',    to: 'users#update',    via: 'post'
+  match '/user/sign_out',    to: 'users#destroy',    via: 'delete'
+  match '/user/point',    to: 'users#point',    via: 'get'
+  match '/store/sign_up',    to: 'stores#new',    via: 'get'
+  match '/store',    to: 'stores#create',    via: 'post'
+  match '/store/update',    to: 'stores#update',    via: 'post'
+  match '/store/index',    to: 'stores#index',    via: 'get'
+  match '/store/sign_out',    to: 'stores#destroy',    via: 'delete'
+
 end
