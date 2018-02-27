9d9d6141e731f57a38bf374f35153d57347f0f2c
[pan16] <[mnsaabc@yahoo.co.jp]>
2018-02-27 14:37:01 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match '/',    to: 'static_pages#home',    via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/user/sign_up',    to: 'users#new',    via: 'get'
-  match '/user',    to: 'users#create',    via: 'post'
-  match '/user/update',    to: 'users#update',    via: 'post'
-  match '/user/sign_out',    to: 'users#destroy',    via: 'delete'
-  match '/user/point',    to: 'users#point',    via: 'get'
-  match '/store/sign_up',    to: 'stores#new',    via: 'get'
-  match '/store',    to: 'stores#create',    via: 'post'
-  match '/store/update',    to: 'stores#update',    via: 'post'
-  match '/store/index',    to: 'stores#index',    via: 'get'
-  match '/store/sign_out',    to: 'stores#destroy',    via: 'delete'
+  root to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+
+  resources :users
+  resources :stores
+  resources :sessions
+
+  get '/user/sign_up',    to: 'users#new'
+  post '/user',    to: 'users#create'
+  put '/user/:id',    to: 'users#update'
+  
+  get '/store/sign_up',    to: 'stores#new'
+  post '/store',    to: 'stores#create'
+  put '/store/:id',    to: 'stores#update'
+  
+  get '/session/log_in',    to: 'sessions#new'
+  post '/session',    to: 'sessions#create'
+  get '/session/log_out',    to: 'sessions#destroy'
 
 end
